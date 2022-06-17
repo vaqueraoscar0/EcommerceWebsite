307474b6bffa008c7599ec280beb556d8e06a8b2
Oscar Vaquera <vaqueraoscar0@gmail.com>
2022-06-17 13:12:00 -0700

    Redux implemented

--- backend/main/views.py
+++ backend/main/views.py
@@ -3,6 +3,7 @@ from .products import products
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Product
+from .serializer import ProductSerializer
 
 # Create your views here.
 @api_view(['GET'])
@@ -23,16 +24,14 @@ def getRoutes(request):
 @api_view(['GET'])
 def getProducts(request):
     products = Product.objects.all()
-    return Response(products)
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
 
-    return Response(product)
+    return Response(serializer.data)
 
