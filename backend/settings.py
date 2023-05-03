a48750a229df4b6a24eab6badfc2c6f036d67d45
Oscar Vaquera <vaqueraoscar0@gmail.com>
2023-05-03 14:26:50 -0700

    requirement.txt file updated

--- backend/settings.py
+++ backend/settings.py
@@ -47,8 +47,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     "corsheaders",
+
+
     'main.apps.MainConfig'
 ]
 
