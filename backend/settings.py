1337c34ed247d4dfb20475b3227036e14496e9bc
Oscar Vaquera <vaqueraoscar0@gmail.com>
2023-05-02 17:20:04 -0700

    static link updated

--- backend/settings.py
+++ backend/settings.py
@@ -171,7 +171,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URLS = '/images/'
+MEDIA_URLS = 'static/images/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
