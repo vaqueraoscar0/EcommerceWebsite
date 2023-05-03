437e9b80902d66551f33cc22eb781ba1857ba85c
Oscar Vaquera <vaqueraoscar0@gmail.com>
2023-05-03 12:16:44 -0700

    static path was added to images

--- backend/settings.py
+++ backend/settings.py
@@ -172,7 +172,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = "/var/www/promozone.herokuapp.com/staticfiles/"
 
 MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images/")
