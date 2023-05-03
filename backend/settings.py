5f2af8b1f300b9603856d17b85f78deb1430ebfe
Oscar Vaquera <vaqueraoscar0@gmail.com>
2023-05-03 14:14:02 -0700

    static files uploaded

--- backend/settings.py
+++ backend/settings.py
@@ -172,9 +172,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "/staticfiles/"
+STATIC_ROOT = BASE_DIR / "/staticfiles/"
 
-MEDIA_URL = '/images/'
+MEDIA_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images/")
 
 STATICFILES_DIRS = [
