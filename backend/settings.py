2d62f90bbec7037d018d20301f3664c76c861bb4
Oscar Vaquera <vaqueraoscar0@gmail.com>
2023-05-03 14:17:09 -0700

    static url chaged

--- backend/settings.py
+++ backend/settings.py
@@ -171,16 +171,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "/staticfiles/"
-
-MEDIA_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "static/images/")
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    BASE_DIR / "frontend/build/static/"
+    BASE_DIR / 'static',
+    BASE_DIR / 'frontend/build/static'
 ]
+
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
