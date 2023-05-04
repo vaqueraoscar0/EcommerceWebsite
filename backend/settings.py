e2389b0365215ae1e7d7c06eeaf4cccfd9470e35
Oscar Vaquera <vaqueraoscar0@gmail.com>
2023-05-04 15:59:49 -0700

    image url

--- backend/settings.py
+++ backend/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = os.getenv('AWS_URL') + "/images/"
+MEDIA_URL = os.getenv('AWS_URL')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
